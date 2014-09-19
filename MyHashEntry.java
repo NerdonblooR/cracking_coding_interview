2a29975955183fd177cced5c51bf58d8f8cf5fbd
NerdonblooR <njbbtan@gmail.com>
2014-09-19 18:07:37 -0400

    "cc150"

--- MyHashEntry.java
+++ MyHashEntry.java
@@ -4,4 +4,31 @@ package cracking_coding_interview;
  * Created by haotan on 2014-09-19.
  */
 public class MyHashEntry {
+
+    private int key;
+    private int value;
+    private MyHashEntry next = null;
+
+    public MyHashEntry(int _key, int _value){
+        key = _key;
+        value = _value;
+    }
+
+    public int getKey(){
+        return key;
+    }
+
+    public int getValue(){
+        return value;
+    }
+
+    public MyHashEntry getNext(){
+        return next;
+    }
+
+    public void append(MyHashEntry _next){
+        next = _next;
+    }
+
+
 }
--- MyHashTable.java
+++ MyHashTable.java
@@ -4,4 +4,56 @@ package cracking_coding_interview;
  * Created by haotan on 2014-09-19.
  */
 public class MyHashTable {
+
+    private static final int capacity = 128;
+
+    private MyHashEntry[] table;
+
+    public MyHashTable(){
+        table = new MyHashEntry[capacity];
+        for (int i=0; i < capacity; i++){
+            table[i] = null;
+        }
+    }
+
+    private int hashFunction(int inputValue){
+        return inputValue % capacity;
+    }
+
+
+    public Integer get(int key){
+        int hash = hashFunction(key);
+        MyHashEntry walker = table[hash];
+        while(walker != null){
+            if (walker.getKey() == key){
+                return walker.getValue();
+            }
+            walker = walker.getNext();
+        }
+        return null;
+    }
+
+    public void put(int key, int value){
+        int hash = hashFunction(key);
+        MyHashEntry newEntry = new MyHashEntry(key,value);
+        MyHashEntry walker = table[hash];
+
+        if (walker == null){
+            table[hash] = newEntry;
+        }else{
+            while (walker.getNext() != null){
+                walker = walker.getNext();
+            }
+            walker.append(newEntry);
+        }
+
+
+    }
+
+
+
+
+
+
+
 }
